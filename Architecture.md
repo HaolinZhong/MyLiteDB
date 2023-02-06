2345a4f744d4d9b928934800bb19979056b4e447
hzhong77 <hzhong77@gatech.edu>
2023-02-06 00:25:47 -0500

    add a "void" SQL compiler and virtual machine. add Architecture.md for illustrating.

---
+++ Architecture.md
@@ -0,0 +1,7 @@
+
+# Architecture
+- This project will clone the architecture of sqlite.
+
+![sqlite Architecture](https://cstack.github.io/db_tutorial/assets/images/arch2.gif)
+
+- The "front-end" of sqlite is a SQL compiler that parses a string and outputs an internal representation called bytecode. 
--- REPL.c
+++ REPL.c
@@ -7,24 +7,43 @@
 #include <string.h>
 
 typedef struct InputContext {
-    char* buffer;
+    char *buffer;
     size_t buffer_size;
     ssize_t input_length;
 } InputContext;
 
-InputContext* new_input_context(){
-    InputContext* input_context = (InputContext*) malloc(sizeof(InputContext));
+typedef enum MetaCommandResult {
+    META_COMMAND_SUCCESS,
+    META_COMMAND_UNRECOGNIZED
+} MetaCommandResult;
+
+typedef enum PrepareStatementResult {
+    PREPARE_STATEMENT_SUCCESS,
+    PREPARE_STATEMENT_UNRECOGNIZED
+} PrepareStatementResult;
+
+typedef enum StatementType {
+    INSERT_STATEMENT,
+    SELECT_STATEMENT
+} StatementType;
+
+typedef struct Statement {
+    StatementType type;
+} Statement;
+
+InputContext *new_input_context() {
+    InputContext *input_context = (InputContext *) malloc(sizeof(InputContext));
     input_context->buffer = NULL;
     input_context->buffer_size = 0;
     input_context->input_length = 0;
     return input_context;
 }
 
-void print_prompt(){
+void print_prompt() {
     printf("MyLiteDB> ");
 }
 
-void read_input(InputContext* input_context) {
+void read_input(InputContext *input_context) {
 
 //    getline() reads an entire line from stream, storing the address
 //    of the buffer containing the text into *lineptr.  The buffer is
@@ -39,27 +58,77 @@ void read_input(InputContext* input_context) {
     }
 
     // getline return a string that ends with \n\0. we want to ignore the \n.
-    input_context->input_length= byte_read-1;
-    input_context->buffer[byte_read-1] = '\0';
+    input_context->input_length = byte_read - 1;
+    input_context->buffer[byte_read - 1] = '\0';
 }
 
-void clean_input_context(InputContext* input_context) {
+void clean_input_context(InputContext *input_context) {
     free(input_context->buffer);
     free(input_context);
 }
 
-int main(int argc, char* argv[]) {
-    InputContext* input_context = new_input_context();
+MetaCommandResult execute_meta_command(InputContext *input_context) {
+    if (strcmp(input_context->buffer, ".exit") == 0) {
+        clean_input_context(input_context);
+        printf("Goodbye!\n");
+        exit(EXIT_SUCCESS);
+    } else {
+        return META_COMMAND_UNRECOGNIZED;
+    }
+}
+
+PrepareStatementResult  prepare_statement(InputContext* input_context, Statement* statement) {
+    if (strncmp(input_context->buffer, "insert", 6) == 0) {
+        statement->type = INSERT_STATEMENT;
+        return PREPARE_STATEMENT_SUCCESS;
+    }
+
+    if (strncmp(input_context->buffer, "select", 6) == 0) {
+        statement->type = SELECT_STATEMENT;
+        return PREPARE_STATEMENT_SUCCESS;
+    }
+
+    return PREPARE_STATEMENT_UNRECOGNIZED;
+}
+
+void execute_statement(Statement* statement) {
+    switch (statement->type) {
+        case INSERT_STATEMENT:
+            printf("Here we will do a insert\n");
+            break;
+        case SELECT_STATEMENT:
+            printf("Here we will do a select\n");
+            break;
+    }
+}
+
+int main(int argc, char *argv[]) {
+    InputContext *input_context = new_input_context();
     while (1) {
         print_prompt();
         read_input(input_context);
 
-        if (strcmp(input_context->buffer, ".exit") == 0) {
-            clean_input_context(input_context);
-            exit(EXIT_SUCCESS);
-        } else {
-            printf("Unrecognized command '%s'.\n", input_context->buffer);
+        if (input_context->buffer[0] == '.') {
+            switch (execute_meta_command(input_context)) {
+                case META_COMMAND_SUCCESS:
+                    continue;
+                case META_COMMAND_UNRECOGNIZED:
+                    printf("Unrecognized command '%s'.\n", input_context->buffer);
+                    continue;
+            }
         }
+
+        Statement statement;
+        switch (prepare_statement(input_context, &statement)) {
+            case PREPARE_STATEMENT_SUCCESS:
+                break;
+            case PREPARE_STATEMENT_UNRECOGNIZED:
+                printf("Unrecognized statement '%s'.\n", input_context->buffer);
+                continue;
+        }
+
+        execute_statement(&statement);
+        printf("Statement executed.\n");
     }
 }
 
